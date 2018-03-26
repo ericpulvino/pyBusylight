f1aa6f751a04d4374596f4d1b3feee8d8196c603
Eric Pulvino <ericpulvino@gmail.com>
2018-03-25 21:31:08 -0400

    Updates for PyPi packaging.

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # pyBusylight
 
+pyBusylight is a native python library which controls Kuando Busylight devices. It is intented to serve as a foundational component which can be integrated into other higher-level scripts.
+
+pyBusylight makes use of PyUSB to control Busylight and used extensive cues from the [busylight node library](https://github.com/porsager/busylight).
+
 ## Dependencies
-* pyusb (pip install pyusb)
+* signal
+* pyusb
 
 ## Most Simple Use Case
 ```
+user@hostname$ sudo python
+
 import pybusylight
 
 test=pybusylight.busylight()
 
-test.send_signal()
+test.send()
 ```
---
+++ setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+from version import __VERSION__
+import io
+
+def read_contents(fname='README'):
+    """read contents of readme into setup.py long_description field
+    """
+    return io.open(os.path.join(os.path.dirname(__file__),
+                                fname), encoding="utf-8").read()
+
+
+setup(
+    name='pyBusylight',
+    version=__VERSION__,
+    url='https://github.com/ericpulvino/pyBusylight',
+    description='Python Library for Kuando Busylight device.',
+    long_description=read_contents(),
+    author='Eric Pulvino',
+    author_email='ericpulvino@gmail.com',
+    install_requires=[
+        'setuptools',
+        'pyusb',
+        'signal'
+    ],
+    license='MIT',
+    classifiers=[
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+    
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
+    
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='kuando busylight',
+    project_urls={
+        'Documentation':'https://github.com/ericpulvino/pyBusylight/docs'
+        'Source':'https://github.com/ericpulvino/pyBusylight',
+        'Tracker':'https://github.com/ericpulvino/pyBusylight/issues'
+    },
+    packages=find_packages(),
+    python_requires='>=2.7',
+)
